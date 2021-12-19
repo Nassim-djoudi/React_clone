5b79c4c9f9213d7babe82451ff4b0a87e8be119f
unknown <nassim.djoudi@imt-atlantique.net>
2021-12-19 20:31:34 +0100

    Api_keys

---
+++ netflix_clone/src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY="72291ba9ccd4cad255b5cc5bb5dac27a";
+
+const requests={
+	fetchTrending:`3/trending/all/day?api_key=${API_KEY}`,
+	fetchNetflix:`3/discover/tv?api_key=${API_KEY}>&language=fr&with_networks=213`,
+	fetchTopRated:`/3/movie/top_rated?api_key=${API_KEY}&language=fr`,
+	fetchActionMovies:`3/discover/tv?api_key=${API_KEY}>&language=fr&with_genres=28`,
+	fetchComedyMovies:`3/discover/tv?api_key=${API_KEY}>&language=fr&with_genres=35`,
+	fetchActionMovies:`3/discover/tv?api_key=${API_KEY}>&language=fr&with_genres=27`,
+	fetchRomanceMovies:`3/discover/tv?api_key=${API_KEY}>&language=fr&with_genres=10749`,
+	fetchDocumentaries:`3/discover/tv?api_key=${API_KEY}>&language=fr&with_genres=99`
+
+}
